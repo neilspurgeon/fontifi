c3446288d7e12a96733b33f32c8c3b8c57b16e17
Neil <neilspurgeon@gmail.com>
2017-07-28 18:30:17 -0700

    started styling focused state

--- src/components/forms/colorPicker/ColorPicker.js
+++ src/components/forms/colorPicker/ColorPicker.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './style.css';
 
-function ColorPicker(props) {
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false
+    };
+    this.onFocus = this.onFocus.bind(this);
+  }
 
-  return (
-    <input
-      type="color"
-      value={props.color}
-      onChange={props.handleChange}
-    />
-  );
+  onFocus() {
+    console.log("focus");
+    this.setState({
+      focused: !this.state.focused
+    });
+  };
+
+  render() {
+    const swatchStyle = {backgroundColor: this.props.value};
+
+    return (
+      <div className={this.state.focused ? styles.focusedClass : styles.ColorPicker}>
+        <label className={styles.CustomColorSwatch} style={swatchStyle}>
+          <input
+            className={styles.NativeColorSwatch}
+            type="color"
+            value={this.props.value}
+            onChange={this.props.handleChange}
+          />
+        </label>
+        <input
+          className={styles.ColorCode}
+          value={this.props.value}
+          onChange={this.props.handleChange}
+        />
+      </div>
+    );
+  }
 };
 
 export default ColorPicker;
