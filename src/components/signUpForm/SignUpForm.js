40f9530548f1a3830552285da5749067dadd49ae
Neil <neilspurgeon@gmail.com>
2017-08-05 17:09:06 -0700

    renders modal on submit

--- src/components/signUpForm/SignUpForm.js
+++ src/components/signUpForm/SignUpForm.js
@@ -3,18 +3,18 @@ import styles from './style.css';
 
 function SignUpForm (props) {
 
-    return (
-      <div>
-        <p>{props.message}</p>
-        <h1>Create Account with</h1>
-        <div className={styles.divider}>or</div>
-        <form>
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
-          <submit>Sign Up</submit>
-        </form>
-      </div>
-    );
+  return (
+    <div>
+      <p>{props.message}</p>
+      <h1>Create Account with</h1>
+      <div className={styles.divider}>or</div>
+      <form>
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="Password" />
+        <submit>Sign Up</submit>
+      </form>
+    </div>
+  );
 }
 
 
--- src/containers/modal/Modal.js
+++ src/containers/modal/Modal.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './style.css';
 import Portal from 'react-portal';
+import { CSSTransitionGroup } from 'react-transition-group';
 
-function Modal (props) {
+class Modal extends Component {
 
-  let isOpen = props.isOpen ? styles.show : styles.hide;
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.overlayClassNames = [styles.overlay];
+    this.reveal = this.reveal.bind(this);
+  }
+
+  componentWillRecieveProps(props) {
+    console.log(props);
+  }
+
+  reveal() {
+    this.overlayClassNames.push(styles.show);
+  }
+
+
+  render() {
+
+    // let overlayClassNames = ;
 
     return (
-      <Portal isOpened={props.isOpen} closeOnEsc={true} >
-        <div className={[styles.overlay, isOpen].join(' ')} onClick={props.closeModal}>
-          <div className={styles.modal}>
-            <button onClick={props.closeModal}>Close</button>
-            { props.children }
-          </div>
-        </div>
-      </Portal>
+
+
+        <Portal isOpened={this.props.isOpen} closeOnEsc={true}>
+
+          <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+
+            <div key={'asdads'} className={this.overlayClassNames.join(' ')} onClick={this.props.closeModal}>
+              <div className={styles.modal}>
+                <button onClick={this.props.closeModal}>Close</button>
+                {this.props.children}
+              </div>
+            </div>
+
+          </CSSTransitionGroup>
+        </Portal>
+
     );
+  }
+
 }
 
 export default Modal;
