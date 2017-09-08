83e61fcb96f71e5c26bd8c93612df190014b3b91
Neil <neilspurgeon@gmail.com>
2017-09-07 21:35:31 -0700

    added redirect and cleaned up commented code

--- src/utils/authService/AuthService.js
+++ src/utils/authService/AuthService.js
@@ -52,9 +52,7 @@ export default class Auth {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        // history.replace('/home');
       } else if (err) {
-        // history.replace('/home');
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
@@ -67,8 +65,6 @@ export default class Auth {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
-    // navigate to the home route
-    // history.replace('/home');
   }
 
   logout() {
@@ -76,7 +72,7 @@ export default class Auth {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
-    // navigate to the home route
+    window.location.href = "/";
   }
 
   isAuthenticated() {
