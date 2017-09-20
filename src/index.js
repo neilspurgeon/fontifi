f3e23cfe9d4db3e9c31b9b7b992913b1742f7d18
Neil <neilspurgeon@gmail.com>
2017-09-19 18:34:20 -0700

    removed service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from 'registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from 'routes';
 import 'index.css';
