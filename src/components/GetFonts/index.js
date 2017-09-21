d96ccf754eb57d544b51e12bb003d27fc15c2301
Neil <neilspurgeon@gmail.com>
2017-09-20 21:05:48 -0700

    styled button and text

--- src/components/GetFonts/index.js
+++ src/components/GetFonts/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './style.css';
 
 function GetFonts(props) {
 
   return(
-    <p className={props.className}>Press <button onClick={props.triggerUpdateFonts}>Spacebar</button> to load new fonts</p>
+    <p className={[styles.text, props.className].join(' ')}>Press <button className={styles.spacebarButton} onClick={props.triggerUpdateFonts}>spacebar</button> to generate new font combinations</p>
   );
 }
 
