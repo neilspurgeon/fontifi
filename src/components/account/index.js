91b6cb9bcc49f38c80f0305420afb1b79af0917b
Neil <neilspurgeon@gmail.com>
2017-09-26 22:37:51 -0700

    menu transitions open

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -28,10 +28,12 @@ class Account extends React.Component {
 
   render() {
     let dropdownStyles = ClassNames(styles.accountDropdown, {[styles.isActive] : this.state.active });
+    let accountContainerStyles = ClassNames(styles.accountContainer, {[styles.isActive] : this.state.active });
+
     return (
-      <div className={styles.accountContainer}>
-        <div className={styles.accountIcon} onMouseOver={this.openMenu} onMouseOut={this.closeMenu}></div>
-        <div className={dropdownStyles}>
+      <div className={accountContainerStyles} onMouseOver={this.openMenu} onMouseOut={this.closeMenu}>
+        <div className={styles.accountIcon}></div>
+        <div className={dropdownStyles} onMouseOut={this.closeMenu}>
           <div className={styles.background}></div>
           <ul className={styles.accountUl}>
             <li>Log Out</li>
