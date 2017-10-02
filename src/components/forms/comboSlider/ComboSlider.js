af59c779fa723ab1876907448c72139c770c98be
Neil <neilspurgeon@gmail.com>
2017-10-01 17:47:56 -0700

    added custom arrows

--- src/components/forms/comboSlider/ComboSlider.js
+++ src/components/forms/comboSlider/ComboSlider.js
@@ -1,54 +1,61 @@
 import React from 'react';
 import styles from './style.css';
 
-function ComboSlider(props) {
+class ComboSlider extends React.Component {
 
-  const stepUp = () => {
-    const currValue = props.value;
-    const stepAmount = Number(props.step);
+  // setFocus = () => {
+  //   this.refs.myInput.focus();
+  // };
+
+  stepUp = () => {
+    const currValue = this.props.value;
+    const stepAmount = Number(this.props.step);
     const newValue = currValue + stepAmount;
     const roundNewValue = Number(newValue.toPrecision(2));
 
-    if (newValue >= props.min && newValue <= props.max) {
-      props.setFontValue(roundNewValue);
+    if (newValue >= this.props.min && newValue <= this.props.max) {
+      this.props.setFontValue(roundNewValue);
     }
   };
 
-  const stepDown = () => {
-    const currValue = props.value;
-    const stepAmount = Number(props.step);
+  stepDown = () => {
+    const currValue = this.props.value;
+    const stepAmount = Number(this.props.step);
     const newValue = currValue - stepAmount;
     const roundNewValue = Number(newValue.toPrecision(2));
 
-    if (newValue >= props.min && newValue <= props.max) {
-      props.setFontValue(roundNewValue);
+    if (newValue >= this.props.min && newValue <= this.props.max) {
+      this.props.setFontValue(roundNewValue);
     }
   };
 
-  return(
-    <span className={styles.ComboSlider}>
-    <input
-      className={styles.Slider}
-      type="range"
-      min={props.min}
-      max={props.max}
-      step={props.step}
-      value={props.value}
-      onChange={props.handleChange}
-    />
-    <input
-      className={styles.Input}
-      onChange={props.handleChange}
-      type="number"
-      min={props.min}
-      max={props.max}
-      step={props.step}
-      value={props.value}
-    />
-    <button type="button" value={props.value} onClick={stepUp} />
-    <button type="button" value={props.value} onClick={stepDown} />
-    </span>
-  );
+  render() {
+    return(
+      <span className={styles.ComboSlider}>
+      <input
+        className={styles.Slider}
+        type="range"
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+        value={this.props.value}
+        onChange={this.props.handleChange}
+      />
+      <input
+        ref="myInput"
+        className={styles.Input}
+        onChange={this.props.handleChange}
+        type="number"
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+        value={this.props.value}
+      />
+      <button className={styles.stepUp} type="button" value={this.props.value}  onClick={this.stepUp} tabIndex="-1" />
+      <button className={styles.stepDown} type="button" value={this.props.value} onClick={this.stepDown} tabIndex="-1" />
+      </span>
+    );
+  }
 };
 
 export default ComboSlider;
