ebc9d3d247bf95f0217562fb1b8ea4fddf2c72fd
Neil <neilspurgeon@gmail.com>
2017-10-01 18:17:08 -0700

    added dropdown arrow, refactored to use same styles

--- src/components/forms/select/Select.js
+++ src/components/forms/select/Select.js
@@ -4,17 +4,19 @@ import styles from './style.css';
 function Select(props) {
 
   return(
-    <select
-      className={styles.Select}
-      onChange={props.handleChange}
-      value={props.value}>
-      { props.options.map((text, index) => {
-        return <option
-          value={text}
-          key={index}
-          data={index}>{text}</option>;
-      })}
-    </select>
+    <div className={styles.selectWrapper}>
+      <select
+        className={styles.Select}
+        onChange={props.handleChange}
+        value={props.value}>
+        { props.options.map((text, index) => {
+          return <option
+            value={text}
+            key={index}
+            data={index}>{text}</option>;
+        })}
+      </select>
+    </div>
   );
 }
 
--- src/components/forms/selectFont/SelectFont.js
+++ src/components/forms/selectFont/SelectFont.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import styles from './style.css';
+import styles from '../select/style.css';
 
 function SelectFont(props) {
 
   return(
-    <select
-      className={styles.Select}
-      onChange={props.handleChange}
-      value={props.value}>
-      { props.options.map((obj, index) => {
-        return <option
-          value={obj[props.keyValue]}
-          key={index}
-          data={index}>{obj[props.keyValue]}</option>;
-      })}
-    </select>
+    <div className={styles.selectWrapper}>
+      <select
+        className={styles.Select}
+        onChange={props.handleChange}
+        value={props.value}>
+        { props.options.map((obj, index) => {
+          return <option
+            value={obj[props.keyValue]}
+            key={index}
+            data={index}>{obj[props.keyValue]}</option>;
+        })}
+      </select>
+    </div>
   );
 }
 
