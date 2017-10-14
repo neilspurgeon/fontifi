137883dfb11e556a02368d6f4cba06e52f779224
Neil <neilspurgeon@gmail.com>
2017-10-14 12:18:40 -0700

    takes optional classnames

--- src/components/forms/button/Button.js
+++ src/components/forms/button/Button.js
@@ -4,7 +4,7 @@ import styles from './style.css';
 function Button(props) {
   return (
     <button
-      className={styles.button}
+      className={ props.className ? [props.className, styles.button].join(' ') : styles.button}
       onClick={props.onClick}
       type={ props.type || 'button'}
       value={props.value}>{props.text}</button>
