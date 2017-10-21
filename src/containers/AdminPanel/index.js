4f6a09a5ddc8ca877b10e9c3cdb5cf79c4585f71
Neil <neilspurgeon@gmail.com>
2017-10-20 23:43:33 -0700

    styling

--- src/containers/AdminPanel/index.js
+++ src/containers/AdminPanel/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FontListItem from 'components/adminPanel/FontListItem';
+import styles from './style.css';
 
 class AdminPanel extends React.Component {
 
@@ -31,18 +32,18 @@ class AdminPanel extends React.Component {
       return (<h1>Empty State</h1>);
     }
     return (
-      <div>
+      <div className={styles.mainContent}>
         <h1>Font List</h1>
-          <div>
-            { this.state.fontList.map((font, index) => {
-              return (
-                <FontListItem
-                  fontFamily={'poppins'}
-                  fontPairs={font.fontPairs}
-                />
-              );
-            })}
-          </div>
+        <div>
+          { this.state.fontList.map((font, index) => {
+            return (
+              <FontListItem
+                fontFamily={'poppins'}
+                fontPairs={font.fontPairs}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
