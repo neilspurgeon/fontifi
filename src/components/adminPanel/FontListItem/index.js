bb38f8051c1077ab55cf09a5915d59f8f4e99f33
Neil <neilspurgeon@gmail.com>
2017-10-20 23:03:44 -0700

    created base admin font list structure

---
+++ src/components/adminPanel/FontListItem/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './style.css';
+import ClassNames from 'classnames';
+import WebFont from 'webfontloader';
+
+class FontListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  };
+
+  componentDidMount() {
+    WebFont.load({
+      google: {
+        families: [this.props.fontFamily]
+      }
+    });
+  }
+
+  render() {
+    let containerClasses = ClassNames(styles.container, {[styles.isActive] : this.props.isActive === true});
+    let fontPairs = this.props.fontPairs;
+
+    const fontStyle = {
+      fontFamily: this.props.fontFamily,
+    };
+
+    return (
+      <div className={containerClasses} >
+        <h2 style={fontStyle}>{this.props.fontFamily}</h2>
+        {fontPairs ? fontPairs.map((font, index) => {
+          return (
+            <div>
+              <p>{font.fontFamily}</p>
+            </div>
+          );
+        }) : null}
+      </div>
+    );
+  }
+}
+
+export default FontListItem;
