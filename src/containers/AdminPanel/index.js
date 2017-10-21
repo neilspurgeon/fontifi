cffab3420feac1463c3c6b178d73a80cec68666b
Neil <neilspurgeon@gmail.com>
2017-10-20 22:39:09 -0700

    gets fontlist and sets as state

--- src/containers/AdminPanel/index.js
+++ src/containers/AdminPanel/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
 class AdminPanel extends React.Component {
-  render() {
-    return (
-      <h1>Admin Panel</h1>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontList: ['asdasd']
+    };
+  }
+
+  componentDidMount = () => {
+    this.setFontList();
   };
+
+  setFontList = () => {
+    fetch('/fonts')
+      .then((res) => {
+        return res.json();
+      })
+      .then((parsedFonts) => {
+        this.setState({
+          fontList: parsedFonts
+        });
+      });
+  };
+
+  render() {
+    if (!this.state) {
+      return (<h1>Empty State</h1>);
+    }
+    return (
+      <div>
+        <h1>Font List</h1>
+          <div>
+            { this.state.fontList.map((obj, index) => {
+              return (<div>{obj.fontFamily}</div>);
+            })}
+          </div>
+      </div>
+    );
+  }
 }
 
 export default AdminPanel;
