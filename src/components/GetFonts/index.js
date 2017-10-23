b70d239a1ab777170998ce7fd79ebfd6f2f4ce15
Neil <neilspurgeon@gmail.com>
2017-10-22 19:07:38 -0700

    Remove text and use button component.

--- src/components/GetFonts/index.js
+++ src/components/GetFonts/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './style.css';
+import Button from 'components/forms/button';
 
 function GetFonts(props) {
 
   return(
-    <p className={[styles.text, props.className].join(' ')}>Press <button className={styles.spacebarButton} onClick={props.triggerUpdateFonts}>spacebar</button> to generate new font combinations</p>
+    <Button onClick={props.triggerUpdateFonts}
+      className={props.className}
+      text="Load New Fonts" />
   );
 }
 
