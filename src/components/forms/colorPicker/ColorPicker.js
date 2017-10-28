46e8755266f4facb85bc537617c43674fb10510d
Neil <neilspurgeon@gmail.com>
2017-10-28 15:55:21 -0700

    Validates hex input and gets rgba values to set.

--- src/components/forms/colorPicker/ColorPicker.js
+++ src/components/forms/colorPicker/ColorPicker.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './style.css';
 import ClassNames from 'classnames';
 import { SketchPicker } from 'react-color';
 import onClickOutside from 'react-onclickoutside';
 
-
-class ColorPicker extends Component {
+class ColorPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -44,7 +43,59 @@ class ColorPicker extends Component {
   }
 
   handleChange = (color) => {
-    this.props.onColorPickerChange(color.rgb, color.hex);
+    const value = { rgba: color.rgb, hex: color.hex };
+    this.props.onColorPickerChange('color', value);
+  }
+
+  handleBlur = (event) => {
+    const value = event.target.value;
+    const validValue = this.convertInput(value);
+    this.toggleFocus();
+
+    // reset to default if not input is entered
+    if (!validValue) {
+      return event.target.value = event.target.defaultValue;
+    }
+    event.target.value = validValue.hex;
+    this.handleChange(validValue);
+  }
+
+  hexToRGB = (hex) => {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+    return {r: r, g: g, b: b, a: 1};
+  }
+
+  shortHandToHex = (color) => {
+  	const digits = color.split('');
+  	let hex = digits[0] + digits[1] + digits[1] + digits[2] + digits[2] + digits[3] + digits[3];
+  	return hex;
+  }
+
+  hexToColorObj = (hex) => {
+    return {
+      rgb: this.hexToRGB(hex),
+      hex: hex
+    };
+  }
+
+  convertInput = (value) => {
+    const length = value.length;
+    if (length >= 7) {
+      value = value.substring(7, 0);
+    } else if (length >= 4) {
+      value = value.substring(4, 0);
+      value = this.shortHandToHex(value);
+    } else {
+      return false;
+    }
+
+    const isColor =  /^#[0-9A-F]{6}$/i.test(value);
+    if (isColor) {
+      return this.hexToColorObj(value);
+    }
+    return false;
   }
 
   render() {
@@ -71,10 +122,9 @@ class ColorPicker extends Component {
         />
         <input
           className={styles.ColorCode}
-          value={this.props.color.hex}
-          onChange={this.props.onInputChange}
+          defaultValue={this.props.color.hex}
           onFocus={this.toggleFocus}
-          onBlur={this.toggleFocus}
+          onBlur={this.handleBlur.bind(this)}
           onMouseEnter={this.toggleHover}
           onMouseOut={this.toggleHover}
         />
