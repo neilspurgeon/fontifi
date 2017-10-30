443d40cb572897565a97179dcb8adfe173027369
Neil <neilspurgeon@gmail.com>
2017-10-29 19:29:46 -0700

    Added google analytics tracking.

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,15 @@ import Callback from 'components/callback/Callback.js';
 import NotFound from './containers/NotFound';
 import AdminPanel from './containers/AdminPanel';
 import Auth from 'utils/authService/AuthService';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-45097231-3');
+
+const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  return null;
+};
 
 const auth = new Auth();
 
@@ -24,14 +33,17 @@ const AdminRoute = ({ component: Component, ...rest }) => (
 );
 
 const Routes = (props) => (
-  <Switch>
-    <Route exact path="/" component={App} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/collection" component={MyCollection} />
-    <Route exact path="/callback" component={Callback} />
-    <AdminRoute exact path="/admin" component={AdminPanel} />
-    <Route component={NotFound} />
-  </Switch>
+  <div>
+    <Route path="/" component={logPageView} />
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/collection" component={MyCollection} />
+      <Route exact path="/callback" component={Callback} />
+      <AdminRoute exact path="/admin" component={AdminPanel} />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
 );
 
 export default Routes;
