4e7b859b0ca06a04d6af012c02b1694ba5ca4c71
Neil <neilspurgeon@gmail.com>
2017-11-26 13:52:27 -0800

    Content and styling

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Navigation from 'containers/navigation';
+import styles from './style.css';
 
 class About extends Component {
   render() {
     return(
-      <div>
-        <Navigation />
-      </div>
+
+        <div className={styles.content}>
+          <Navigation />
+          <h1 className={styles.h1}>This site is an ongoing side project, designed and developed by <a href="http://neilspurgeon.com/">Neil Spurgeon</a>.</h1>
+        </div>
     );
   }
 }
