3a281eb7308399330ab8a24bccea804a311ea2a9
Neil <neilspurgeon@gmail.com>
2017-11-28 21:45:14 -0800

    updated copy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  defaultHeadingText: 'Generate & Collect the Best Google Font Pairings',
+  defaultHeadingText: 'Generate & Discover the Best Google Font Combinations',
   defaultBodyText: 'Press spacebar or click the button below to load new font combinations. Use the editor panel on the right to fine tune or to manually pick new fonts. When you find something you like, save your font pair for easy reference later. All this text is editable and can be replaced with whatever you want.',
   savedBodyText: 'In publishing and graphic design, placeholder text is commonly used to demonstrate the elements of a document or visual presentation, such as font, typography, and layout. Even though using "lorem ipsum" often arouses curiosity because of its resemblance to classical Latin, it is not intended to have meaning.'
 };
