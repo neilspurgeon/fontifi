3c123d2350ed8ec4c748b2b705125f8662677835
Neil <neilspurgeon@gmail.com>
2017-12-31 12:23:20 -0800

    linting

--- src/components/Font/index.js
+++ src/components/Font/index.js
@@ -14,6 +14,7 @@ class Font extends React.Component {
   };
 
   componentWillReceiveProps(nextProps) {
+
     // Only run if props are different
     if ((nextProps.fontFamily !== this.props.fontFamily) || (nextProps.fontWeight !== this.props.fontWeight)) {
       WebFont.load({
@@ -24,20 +25,20 @@ class Font extends React.Component {
         // use fontactive instead of active because of a bug that prevents the 2nd instance from calling
         // https://github.com/typekit/webfontloader/issues/345
         fontactive: () => {
+
           // prevents flash of unstyled text by waiting to for font to load before changing
           this.setState({ fontFamily: nextProps.fontFamily });
-          console.log('font loaded and state set');
         }
       });
     }
   }
 
   render() {
-    let classes = ClassNames(styles.font, {[styles.isActive] : this.props.isActive});
-    // const text = config.defaultHeadingText;
-    const text = this.props.text;
 
+    let classes = ClassNames(styles.font, {[styles.isActive] : this.props.isActive});
+    const text = this.props.text;
     const color = this.props.color.rgba;
+
     const fontStyle = {
       fontFamily: this.state.fontFamily,
       fontSize: this.props.fontSize + 'px',
